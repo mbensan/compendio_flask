0cf081cdc8adc80d56575fca9dd8e8396b0b58f3
Matias Bensan A <mbensan@gmail.com>
2022-12-06 22:08:58 -0300

    Base para proyectos Flsk

---
+++ server.py
@@ -0,0 +1,18 @@
+from app import app
+
+# registramos los controladores de la app
+from app.controllers.auth import auth
+
+app.register_blueprint(auth)
+'''
+from app.controllers.azar import azar
+from app.controllers.twitter import twitter
+from app.controllers.books import books
+
+app.register_blueprint(azar)
+app.register_blueprint(twitter, url_prefix='/twitter')
+app.register_blueprint(books, url_prefix='/books')
+
+'''
+if __name__=="__main__":   
+    app.run(debug=True)    
